1e1029dfe52a61ea742d9b6ebfb6f46167369f03
Andrew Cholakian <andrew@andrewvc.com>
2019-07-25 20:56:00 -0500

    Make hound happy

--- internal/llreflect/chase.go
+++ internal/llreflect/chase.go
@@ -4,6 +4,7 @@ import (
 	"reflect"
 )
 
+// ChaseValue takes a value and returns the underlying type even if it is nested inpointers or wrapped in interface{}
 func ChaseValue(v reflect.Value) reflect.Value {
 	for (v.Kind() == reflect.Ptr || v.Kind() == reflect.Interface) && !v.IsNil() {
 		v = v.Elem()
