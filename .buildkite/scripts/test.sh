5ed90886367f3f13de005176fd1d7be0042d5d3f
Svetlana <5437289+leo-ri@users.noreply.github.com>
2023-08-24 15:20:54 +0200

    naming reports

--- .buildkite/scripts/test.sh
+++ .buildkite/scripts/test.sh
@@ -7,10 +7,10 @@ add_bin_path
 with_go_junit_report
 
 echo "--- Run the tests"
-export OUT_FILE="build/test-report.out"
+export OUT_FILE="build/test-report-${GO_VERSION}"
 mkdir -p build
 set +e
-go test -v -race ./... > ${OUT_FILE}
+go test -v -race ./... > "${OUT_FILE}"
 status=$?
 
 # Buildkite collapse logs under --- symbols
