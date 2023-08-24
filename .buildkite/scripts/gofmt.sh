af98ee7dde6b608392e27aed87e937fc21377cc2
Svetlana <5437289+leo-ri@users.noreply.github.com>
2023-08-24 19:11:19 +0200

    split gofmt

--- .buildkite/scripts/gofmt.sh
+++ .buildkite/scripts/gofmt.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-if [ "${GO_VERSION:-}" = '1.12' ]; then
-
-    if [ -z "$(gofmt -d .)" ]; then
-        true
-    else
-        gofmt -d . && false
-    fi
-
+if [ -z "$(gofmt -d .)" ]; then
+    true
+else
+    gofmt -d . && false
 fi
